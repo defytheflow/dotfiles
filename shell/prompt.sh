226e4dcb20b9c84bd0a6f050f33543f8ad7d574a
Artyom Danilov <defytheflow@gmail.com>
2020-03-23 00:40:59 +0300

    Show unstaged and modified files in prompt

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/sh
+#!/usr/bin/zsh
 
 # ------------------------------------------------------------------------------
 # | File:      prompt.sh
 # ------------------------------------------------------------------------------
-# | Modified:  March 20, 2020
+# | Modified:  March 23, 2020
 # ------------------------------------------------------------------------------
 # | Author:    Artyom Danilov
 # ------------------------------------------------------------------------------
@@ -11,9 +11,10 @@
 echo "Running ${0}"
 
 # ------------------------------------------------------------------------------
-#                                    Colors
+#                         Shell-dependent Definitions
 # ------------------------------------------------------------------------------
 
+# 'BASH_VERSION' only defined in bash.
 if [ -n "${BASH_VERSION}" ]; then
 
     start_color='\[\e[38;5;'
@@ -22,6 +23,15 @@ if [ -n "${BASH_VERSION}" ]; then
     nl='\n'
     reset='\[\e[0m\]'
 
+    username='\u'
+    hostname='\h'
+    absdir='\w'
+    reldir='\W'
+
+    # 'PROMPT_COMMAND' is a special environment variable name known to bash.
+    PROMPT_COMMAND=my_prompt
+
+# 'ZSH_VERSION' only defined in zsh.
 elif [ -n "${ZSH_VERSION}" ]; then
 
     start_color=$'%{\e[38;5;'
@@ -29,7 +39,19 @@ elif [ -n "${ZSH_VERSION}" ]; then
 
     nl=$'\n'
     reset=$'%{\e[0m%}'
+
+    username='%n'
+    hostname='%M'
+    absdir='%~'
+    reldir='%1~'
+
+    # 'precmd' is a special function name known to zsh.
+    precmd() { my_prompt; }
 fi
+
+# ------------------------------------------------------------------------------
+#                                    Colors
+# ------------------------------------------------------------------------------
 
 cyan="${start_color}45${end_color}"
 green="${start_color}82${end_color}"
@@ -38,79 +60,44 @@ orange="${start_color}202${end_color}"
 purple="${start_color}135${end_color}"
 red="${start_color}9${end_color}"
 white="${start_color}15${end_color}"
-
-# ------------------------------------------------------------------------------
-#                              Special Variables
-# ------------------------------------------------------------------------------
-
-if [ -n "${BASH_VERSION}" ]; then
-
-    username='\u'
-    hostname='\h'
-    absdir='\w'
-    reldir='\W'
-
-elif [ -n "${ZSH_VERSION}" ]; then
-
-    username='%n'
-    hostname='%M'
-    absdir='%~'
-    reldir='%1~'
-fi
-
-# ---------------------------------------------------------------------------- #
-#                                   Statuses                                   #
-# ---------------------------------------------------------------------------- #
-
-success="${green}:)${reset}"
-failure="${red}:(${reset}"
-
-committed="${green}✓${reset}"
-uncommitted="${red}✗${reset}"
+yellow="${start_color}220${end_color}"
 
 # ---------------------------------------------------------------------------- #
 #                                    Prompt                                    #
 # ---------------------------------------------------------------------------- #
-
-# 'BASH_VERSION' only defined in Bash
-# 'PROMPT_COMMAND' is a special environment variable name known to Bash
-[ -n "${BASH_VERSION}" ] && PROMPT_COMMAND=my_prompt
-
-# 'ZSH_VERSION' only defined in Zsh
-# 'precmd' is a special function name known to Zsh
-[ -n "${ZSH_VERSION}" ] && precmd() { my_prompt; }
 
 # Gets executed each time prompt is drawn.
 my_prompt() {
 
     # Get last command status
     if [ $? -eq 0 ]; then
-        command_status=" ${success}"
+        command_status=" ${green}:)${reset}"
     else
-        command_status=" ${failure}"
+        command_status=" ${red}:(${reset}"
     fi
 
     # Get git branch
     if git status >/dev/null 2>&1; then
+
         # Branch currently on.
         branch="$(git branch 2>/dev/null | grep "^\*" | colrm 1 2)"
+        # Number of unstaged files.
+        unstaged="$(git status --porcelain | grep '?' | wc -l)"
+        # Number of modified files.
+        modified="$(git status --porcelain | grep 'M' | wc -l)"
+        # Total number of commits in repository on branch.
+        total="$(git rev-list --count "${branch}")"
 
-        # Number of commits in repository on branch.
-        commits="$(git rev-list --count "${branch}")"
-
-        if [ -z "$(git status -s)" ]; then
-            git_status="${committed}"
-        else
-            git_status="${uncommitted}"
-        fi
-
-        # Colorize branch and commits.
+        # Colorize.
         branch="${cyan}${branch}"
         sep="${white}|"
-        commits="${cyan}${commits}"
+        unstaged="${red}${unstaged}${reset}"
+        modified="${yellow}${modified}${reset}"
+        total="${green}${total}"
 
-        # Putting all together.
-        branch=" ${white}(${branch}${sep}${commits}${sep}${git_status}${white} )"
+        # Put all together.
+        stats="${white}[${unstaged}-${modified}-${total}${white}]"
+        branch="${white}${branch} ${stats}${white}"
     else
         branch=''
     fi;
@@ -119,7 +106,7 @@ my_prompt() {
     PS1="${nl}${purple}${username}"             # username
     PS1+="${white} at ${orange}${hostname}"     # at host
     PS1+="${white} in ${limegreen}${absdir}"    # in directory
-    PS1+="${branch}"                            # (branch|commits)
+    PS1+="${white} on ${branch}"                        # (branch|commits)
     PS1+="${command_status}"                    # :) or :(
     PS1+="${reset}${nl}\$ "
 }
