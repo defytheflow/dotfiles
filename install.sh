8f7fd4f8829205c469f48f37ba09a4cc4a68a374
Artyom Danilov <defytheflow@gmail.com>
2020-03-26 18:14:52 +0300

    Add zsh plugin installation to isntall.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-VIM_PLUG_DEST = '~/.vim/autoload/plug.vim'
-VIM_PLUG_URL =  'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Create zsh-plugins folder.
+[ ! -d "${ZSH_PLUGINS}" ] && mkdir "${ZSH_PLUGINS}"
 
-# Copy all other files except for this.
-sudo find . -maxdepth 1 -type f \( ! -name "Makefile" \) -exec cp -i {} ~/ \;
-# Copy all files from 'bash/'.
-sudo find bash python tmux zsh -maxdepth 1 -type f -exec cp -i {} ~/ \;
-# Copy all files from 'vim/'.
-sudo find vim -maxdepth 1 -type f -exec cp -i {} ~/ \;
-# Install vim-plug
-if [[ ! -f $(VIM_PLUG_DEST) ]]; then \
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs $(VIM_PLUG_URL) > /dev/null 2>&1; \
-fi;
+[ ! -d "${ZSH_PLUGINS}"/zsh-syntax-highlighting ] && \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    "${ZSH_PLUGINS}"/zsh-syntax-highlighting
+
+[ ! -d "${ZSH_PLUGINS}"/zsh-autosuggestions ] && \
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+    "${ZSH_PLUGINS}"/zsh-autosuggestions
+
+[ ! -d "${ZSH_PLUGINS}"/alias-tips ] && \
+    git clone https://github.com/djui/alias-tips.git \
+    "${ZSH_PLUGINS}"/alias-tips
