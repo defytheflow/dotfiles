2a4f6045f27b3d1283ab3cf8765bcacc0954135a
Artyom Danilov <defytheflow@gmail.com>
2020-03-30 19:00:17 +0300

    Drastically improve zsh directory
    Leverage zplug to handle plugin installation.
    Leverage spaceship-prompt to customize prompt.
    Add .zshenv file to setup all the essential environment variables.

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@
 # Load essential environment variables.
 . "$(pwd)"/profile
 
-
 create_symlinks() {
     ln -sf "${DOTFILES_HOME}"/profile         "${HOME}"/.profile
     ln -sf "${DOTFILES_HOME}"/xprofile        "${HOME}"/.xprofile
@@ -22,30 +21,4 @@ create_symlinks() {
     ln -sf "${DOTFILES_HOME}"/python/pythonrc "${XDG_CONFIG_HOME}"/python/pythonrc
 }
 
-
-download_zsh_plugins() {
-    [ ! -d "${ZSH_PLUGINS}" ] && mkdir "${ZSH_PLUGINS}"
-
-    [ ! -d "${ZSH_PLUGINS}"/zsh-syntax-highlighting ] && \
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-        "${ZSH_PLUGINS}"/zsh-syntax-highlighting
-
-    [ ! -d "${ZSH_PLUGINS}"/zsh-autosuggestions ] && \
-        git clone https://github.com/zsh-users/zsh-autosuggestions \
-        "${ZSH_PLUGINS}"/zsh-autosuggestions
-
-    [ ! -d "${ZSH_PLUGINS}"/alias-tips ] && \
-        git clone https://github.com/djui/alias-tips.git \
-        "${ZSH_PLUGINS}"/alias-tips
-
-    [ ! -d "${ZSH_PLUGINS}"/command-not-found ] && \
-        mkdir "${ZSH_PLUGINS}"/command-not-found && \
-        url=https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master && \
-        url="${url}"/plugins/command-not-found/command-not-found.plugin.zsh && \
-        wget -P "${ZSH_PLUGINS}"/command-not-found "${url}"
-}
-
-
 create_symlinks
-[ "${SHELL}" = /usr/bin/zsh ] && download_zsh_plugins
-exit 0
---
+++ zshenv
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# File:      .zshenv
+# Purpose:   This file is sourced every zsh session (login, interactive).
+#            Here we load all essential environment variables.
+# Created:   30.03.2020
+# Modified:  30.03.2020
+# Author:    Artyom Danilov
+
+
+. "${HOME}"/.profile
