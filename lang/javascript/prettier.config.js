098e75c1485ad295417be163bd9a9c69d029559b
Artyom Danilov <defytheflow@gmail.com>
2022-05-30 22:06:11 +0300

    add prettier.config.js, rename js to javascript
    * Remove ts folder.
    * Add vim.handleKeys option to vscode, that ignores <C-g> and <C-j>
      binds.

---
+++ lang/javascript/prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Include parentheses around a sole arrow function parameter.
+  arrowParens: 'avoid',
+  // Use single quotes instead of double quotes in JSX.
+  jsxSingleQuote: true,
+  // The line length the printer will wrap on.
+  printWidth: 90,
+  // Change when properties in objects are quoted.
+  quoteProps: 'consistent',
+  // Print semicolons at the ends of statements.
+  semi: false,
+  // Use single quotes instead of double quotes.
+  singleQuote: true,
+}
