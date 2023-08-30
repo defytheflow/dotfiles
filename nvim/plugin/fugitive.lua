813457de50d32ed541dd7656f11bcb5291bcaeeb
Artyom Danilov <defytheflow@gmail.com>
2023-08-30 20:12:30 +0300

    refactor(nvim): add descriptions to fugitive mappings
    • Add `gstsh` (git stash show) alias.

--- nvim/plugin/fugitive.lua
+++ nvim/plugin/fugitive.lua
@@ -1,6 +1,7 @@
 vim.opt.previewheight = 22
 
-vim.keymap.set("n", "<leader>gc", function() vim.cmd.Git("commit") end)
-vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
-vim.keymap.set("n", "<leader>gl", function() vim.cmd.Git("log") end)
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gc", function() vim.cmd.Git("commit") end, { desc = "[G]it [C]ommit" })
+vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff, { desc = "[G]it [D]iff" })
+vim.keymap.set("n", "<leader>gl", function() vim.cmd.Git("log") end, { desc = "[G]it [l]og" })
+vim.keymap.set("n", "<leader>gL", vim.cmd.GV, { desc = "[G]it [L]og (oneline)" }) -- from gv.vim, depends on vim-fugitive
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
