cf222c7360339a6f520240785e4fc5facb1831c3
Artyom Danilov <defytheflow@gmail.com>
2023-09-01 11:10:39 +0300

    refactor(nvim): add descriptions to test mappings

--- nvim/plugin/test.lua
+++ nvim/plugin/test.lua
@@ -3,8 +3,8 @@ vim.g["test#neovim#term_position"] =  "below 40" -- [vert, below]
 vim.g["test#javascript#runner"] = "jest"
 vim.g["test#javascript#jest#executable"] = "npm test"
 
-vim.keymap.set("n", "<leader>tf", vim.cmd.TestFile)
-vim.keymap.set("n", "<leader>tl", vim.cmd.TestLast)
-vim.keymap.set("n", "<leader>tn", vim.cmd.TestNearest)
-vim.keymap.set("n", "<leader>ts", vim.cmd.TestSuite)
-vim.keymap.set("n", "<leader>tv", vim.cmd.TestVisit)
+vim.keymap.set("n", "<leader>tf", vim.cmd.TestFile, { desc = "[T]est [F]ile" })
+vim.keymap.set("n", "<leader>tl", vim.cmd.TestLast, { desc = "[T]est [L]ast" })
+vim.keymap.set("n", "<leader>tn", vim.cmd.TestNearest, { desc = "[T]est [N]earest" })
+vim.keymap.set("n", "<leader>ts", vim.cmd.TestSuite, { desc = "[T]est [S]uite" })
+vim.keymap.set("n", "<leader>tv", vim.cmd.TestVisit, { desc = "[T]est [V]isit" })
