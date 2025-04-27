281dcde69ab7e81412f77abb2677b67881a54380
Artyom Danilov <defytheflow@gmail.com>
2025-04-27 12:37:54 +0300

    feat(nvim): add `gD` keymap to go to definition in vertical split ✨

--- xdg_config/nvim/plugin/lsp.lua
+++ xdg_config/nvim/plugin/lsp.lua
@@ -67,6 +67,11 @@ local on_attach = function(_, bufnr)
   end, "Peek Definition")
 
   nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gD", function()
+    vim.cmd.vsplit()
+    vim.lsp.buf.definition()
+  end, "[G]oto [D]efinition in vertical split")
+
   nmap("gr", function()
     require("telescope.builtin").lsp_references {
       fname_width = 50,
@@ -95,7 +100,6 @@ local on_attach = function(_, bufnr)
   nmap("K", vim.lsp.buf.hover, "Hover Documentation")
   nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
   nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
   nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
   nmap("<leader>wl", function()
